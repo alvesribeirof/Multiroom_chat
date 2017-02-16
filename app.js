12499f43afc1cc629e075c56b5a376b45a0855d6
Fábio Alves Ribeiro <alvesribeirof@gmail.com>
2017-02-16 10:08:30 -0200

    Multiroom_chat
    Bate-papo estilo whatsapp web

---
+++ app.js
@@ -0,0 +1,20 @@
+/* Importar as configurações do servidor*/
+var app = require('./config/server');
+
+/* parametrizar a porta de escuta */
+var server = app.listen(81, function () {
+	console.log('Servidor online');
+})
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket) {
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function() {
+		console.log('Usuário desconectou');
+	});
+});
